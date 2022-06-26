88acf61ed79517a770ab1e1e1e6f1225d91c968e
michael <michael@wohlfart.net>
2022-06-26 12:57:50 +0200

    cleanup readme

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,9 @@ plugins {
 //  some help to get stated
 //
 tasks.register<DefaultTask>("info") {
-    println("-- some commandlines for this project --")
-    println("   * ./gradlew build: create the artifacts")
-    println("   * ./gradlew mrproper: restores the status at checkout, removing any cached or built artifacts")
+    println(" *********************************************************** ")
+    println("   some commandlines for this project --")
+    println("    ./gradlew mrproper        reset the project to it's checkout state by removing all artifacts")
+    println("    ./gradlew build           build all modules and prepare images for deployment")
+    println(" *********************************************************** ")
 }
