2a77ada419ddb9cde61eb9ef3f6808cbef5d3d3e
michael <michael@wohlfart.net>
2022-07-17 19:31:14 +0200

    disable angular routes filter for now

--- backend/src/main/kotlin/net/wohlfart/charon/config/AngularRoutesFilter.kt
+++ backend/src/main/kotlin/net/wohlfart/charon/config/AngularRoutesFilter.kt
@@ -7,25 +7,43 @@ import javax.servlet.Filter
 import javax.servlet.FilterChain
 import javax.servlet.ServletRequest
 import javax.servlet.ServletResponse
+import javax.servlet.http.HttpServletRequest
 
 
-@Component
+fun requestUrl(request: ServletRequest?): String? {
+    return if (request !is HttpServletRequest) null else (request as HttpServletRequest?)?.let {
+        getCurrentUrlFromRequest(
+            it
+        )
+    }
+}
+
+fun getCurrentUrlFromRequest(request: HttpServletRequest): String? {
+    val requestURL = request.requestURL
+    request
+    val queryString = request.queryString ?: return requestURL.toString()
+    return requestURL.append('?').append(queryString).toString()
+}
+
+// @Component
 class AngularRoutesFilter : Filter {
     var logger = Logger.getLogger(this.javaClass.name)
 
 
+    val CHARON_PATH = "/charon"
     val API_PATH = "/api"
     val SWAGGER_CONFIG_PATH = "/v3/api-docs"
     val INDEX_HTML = "/index.html"
 
     override fun doFilter(request: ServletRequest, response: ServletResponse, filterchain: FilterChain) {
-        return if (isAngularRoute(request.servletContext.contextPath)) {
+        return if (isAngularRoute(requestUrl(request))) {
             request.getRequestDispatcher(INDEX_HTML).forward(request,response);
         } else filterchain.doFilter(request, response);
     }
 
-    private fun isAngularRoute(path: String): Boolean {
-        val result = (!path.contains(".") // this indicates a html,jpg,js file we don't want to reroute that
+    private fun isAngularRoute(path: String?): Boolean {
+        val result = (path != null)
+            && (!path.contains(".") // this indicates a html,jpg,js file we don't want to reroute that
             && !path.startsWith(API_PATH)
             && !path.startsWith(SWAGGER_CONFIG_PATH))
         if (result) {
