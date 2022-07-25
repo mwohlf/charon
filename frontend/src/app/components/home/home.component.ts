ed3926a4b2ff1b09606fa27bbac7035a67907f61
michael <michael@wohlfart.net>
2022-07-25 22:44:38 +0200

    first endpoint

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import { BuildService } from 'build/generated/api/api';
+import { BuildProperties } from 'build/generated/model/models';
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-home',
@@ -9,10 +12,16 @@ export class HomeComponent implements OnInit {
 
   static ROUTER_PATH: string = "home";
 
-  constructor() {
+  buildProperties$: Observable<BuildProperties>;
+
+  constructor(
+    private buildService: BuildService
+  ) {
+    this.buildProperties$ = this.buildService.readBuildProperties();
   }
 
   ngOnInit(): void {
+    this.buildService.readBuildProperties()
   }
 
 }
