08d643da8782adae82fd9c8eab28e69e0f6a0dfb
michael <michael@wohlfart.net>
2022-07-31 14:45:34 +0200

    fix ports and probe endpoints

--- oauth/build.gradle.kts
+++ oauth/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 
     implementation("org.springframework.security:spring-security-oauth2-authorization-server:${Versions.SPRING_OAUTH2_SERVER}")
 
