9640b6f954744b28fab478a6fe0e2438e0ebc548
michael <michael@wohlfart.net>
2022-08-09 00:10:34 +0200

    cleanup, waiting for digest

--- backend/src/main/kotlin/net/wohlfart/charon/CharonProperties.kt
+++ backend/src/main/kotlin/net/wohlfart/charon/CharonProperties.kt
@@ -45,3 +45,53 @@ class ClientConfig(
     val clientId: String,
 
 )
+
+
+/*
+
+{
+    "issuer": "http://oauth.finalrestingheartrate.com",
+    "authorization_endpoint": "http://oauth.finalrestingheartrate.com/oauth2/authorize",
+    "token_endpoint": "http://oauth.finalrestingheartrate.com/oauth2/token",
+    "token_endpoint_auth_methods_supported": [
+        "client_secret_basic",
+        "client_secret_post",
+        "client_secret_jwt",
+        "private_key_jwt"
+    ],
+    "jwks_uri": "http://oauth.finalrestingheartrate.com/oauth2/jwks",
+    "userinfo_endpoint": "http://oauth.finalrestingheartrate.com/userinfo",
+    "response_types_supported": [
+        "code"
+    ],
+    "grant_types_supported": [
+        "authorization_code",
+        "client_credentials",
+        "refresh_token"
+    ],
+    "revocation_endpoint": "http://oauth.finalrestingheartrate.com/oauth2/revoke",
+    "revocation_endpoint_auth_methods_supported": [
+        "client_secret_basic",
+        "client_secret_post",
+        "client_secret_jwt",
+        "private_key_jwt"
+    ],
+    "introspection_endpoint": "http://oauth.finalrestingheartrate.com/oauth2/introspect",
+    "introspection_endpoint_auth_methods_supported": [
+        "client_secret_basic",
+        "client_secret_post",
+        "client_secret_jwt",
+        "private_key_jwt"
+    ],
+    "subject_types_supported": [
+        "public"
+    ],
+    "id_token_signing_alg_values_supported": [
+        "RS256"
+    ],
+    "scopes_supported": [
+        "openid"
+    ]
+}
+
+ */
