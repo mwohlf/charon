3bb13127b2f49c2d736d8a932bafef0414098ac7
michael <michael@wohlfart.net>
2022-09-11 21:27:53 +0200

    booting oauth

--- oauth/src/main/kotlin/net/wohlfart/charon/OAuthProperties.kt
+++ oauth/src/main/kotlin/net/wohlfart/charon/OAuthProperties.kt
@@ -4,6 +4,8 @@ package net.wohlfart.charon
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.boot.context.properties.NestedConfigurationProperty
+import org.springframework.security.oauth2.core.AuthorizationGrantType
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod
 
 
 @ConstructorBinding
@@ -18,5 +20,23 @@ class OAuthProperties(
     val allowedOrigins: Array<String>,
 
     @NestedConfigurationProperty
+    val redirectUris: Array<String>,
+
+    @NestedConfigurationProperty
+    val clientRegistry: ClientRegistry,
+)
+
+@ConfigurationProperties
+class ClientRegistry: HashMap<String, ClientEntry>()
+
+@ConstructorBinding
+class ClientEntry (
+
+    val clientAuthenticationMethod: ClientAuthenticationMethod,
+
+    val authorizationGrantType: AuthorizationGrantType,
+
+    val scope: Array<String>,
+
     val redirectUris: Array<String>,
 )
