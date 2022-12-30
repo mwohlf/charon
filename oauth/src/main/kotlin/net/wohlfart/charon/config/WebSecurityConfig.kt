b8dc557ab29df891ffda89d31b966f13fdfbba65
michael <michael@wohlfart.net>
2022-12-30 16:51:16 +0100

    plain default login for now

--- oauth/src/main/kotlin/net/wohlfart/charon/config/WebSecurityConfig.kt
+++ oauth/src/main/kotlin/net/wohlfart/charon/config/WebSecurityConfig.kt
@@ -2,52 +2,47 @@ package net.wohlfart.charon.config
 
 import mu.KotlinLogging
 import net.wohlfart.charon.OAuthProperties
-import net.wohlfart.charon.component.LoginCustomizer
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer.withDefaults
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.web.SecurityFilterChain
-import jakarta.servlet.http.HttpServletRequest
+
 
 private val logger = KotlinLogging.logger(AuthorizationServerConfig::class.java.name)
 
 
 @EnableWebSecurity
+@Configuration(proxyBeanMethods = false)
 class WebSecurityConfig {
 
     @Bean
+    @Throws(Exception::class)
     fun defaultSecurityFilterChain(
         http: HttpSecurity,
-        userDetailsService: UserDetailsService,
-        loginCustomizer: LoginCustomizer,
         oAuthProperties: OAuthProperties,
     ): SecurityFilterChain {
+        http
+            .authorizeHttpRequests { authorize: AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry
+                ->
+                authorize.anyRequest().authenticated()
+            }
 
-        // info about where static resources are served from:
-        // https://spring.io/blog/2017/09/15/security-changes-in-spring-boot-2-0-m4
-        http.authorizeRequests { authorizeRequests ->
-            authorizeRequests
-                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                .requestMatchers("/error").permitAll()
-                .requestMatchers("/bounce").permitAll()
-                .requestMatchers("/login").permitAll()
-                .requestMatchers("/h2/**").permitAll()
-                // .mvcMatchers("/revoke").permitAll()
-                // .mvcMatchers("/oauth2/revoke").anonymous()
-                .anyRequest().authenticated()
-        }
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER)
 
-        http.cors { } // picks up our default cors config
+        http.cors { } // picks up our default cors config for the token endpoint
+        http.formLogin(withDefaults())
+        // http.formLogin(loginCustomizer)
+
+        http.logout().logoutUrl("/logout").permitAll().clearAuthentication(true)
+            .logoutSuccessUrl(oAuthProperties.postLogoutRedirect)
+
         http.csrf { csrf -> csrf.disable() } // for the h2 console
         http.headers().frameOptions().sameOrigin() // which uses frames it seems
-        http.formLogin(loginCustomizer)
-        http.logout().logoutUrl("/logout").permitAll().clearAuthentication(true).logoutSuccessUrl(oAuthProperties.postLogoutRedirect)
+
         return http.build()
-
     }
-
 }
