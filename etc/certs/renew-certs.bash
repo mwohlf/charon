4c252b748a05865a1b962c6e63cb50397dd05c87
michael <michael@wohlfart.net>
2023-01-21 17:34:44 +0100

    action updates

--- etc/certs/renew-certs.bash
+++ etc/certs/renew-certs.bash
@@ -76,14 +76,16 @@ function create_cert() {
 
     sudo ls -alR "${SCRIPT_DIR}"
 
-    sudo gpg --quiet --batch --yes --encrypt \
+    sudo gpg --quiet --batch --yes \
         --passphrase="${GPG_PASSPHRASE}" \
         --output "${SCRIPT_DIR}/tls.crt.gpg" \
+        --symmetric \
         "${SCRIPT_DIR}/etc/live/${DOMAIN}/fullchain.pem"
 
-    sudo gpg --quiet --batch --yes --encrypt \
+    sudo gpg --quiet --batch --yes \
         --passphrase="${GPG_PASSPHRASE}" \
         --output "${SCRIPT_DIR}/tls.key.gpg" \
+        --symmetric \
         "${SCRIPT_DIR}/etc/live/${DOMAIN}/privkey.pem"
 
     sudo cat "${SCRIPT_DIR}/log/letsencrypt/letsencrypt.log"
