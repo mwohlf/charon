d2dd8cfa16826f890d78b6950dc5759832795f11
Michael Wohlfart <michael@wohlfart.net>
2023-04-28 05:08:06 +0200

    using 3.0.6

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-noarg:${jetbrainKotlinVersion}") // for JPA entities
 
     // https://docs.spring.io/spring-boot/docs/current/gradle-plugin/reference/htmlsingle/
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.0.5")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.0.6")
     // https://plugins.gradle.org/plugin/io.spring.dependency-management
     implementation("io.spring.gradle:dependency-management-plugin:1.1.0")
     // implementation("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.6.21-1.0.6")
