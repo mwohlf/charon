4246566cb15ed1c6d646756ff9a8573bc24123f4
michael <michael@wohlfart.net>
2023-07-26 18:45:59 +0200

    update to spring_auth-server 1.2.0-SNAPSHOT

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -9,8 +9,8 @@ object Versions {
 
 
     // https://docs.spring.io/spring-authorization-server/docs/1.0.0-SNAPSHOT/reference/html/
-    // const val SPRING_OAUTH2_SERVER = "1.0.1"
-    const val SPRING_OAUTH2_SERVER = "1.1.0-M1"
+    // const val SPRING_OAUTH2_SERVER = "1.1.0-M1"
+    const val SPRING_OAUTH2_SERVER = "1.2.0-SNAPSHOT"
 
     // https://mvnrepository.com/artifact/io.github.microutils/kotlin-logging-jvm
     const val KOTLIN_LOGGING_JVM = "3.0.5"
